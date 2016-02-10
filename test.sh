78767227e8fcb4be30801a899ac00c89a5675314
Matt Clay <matt@mystile.com>
2016-02-10 01:50:19 -0800

    Add multiple encodings for tests.

--- test.sh
+++ test.sh
@@ -1,7 +1,13 @@
 #!/bin/sh -e
 # Run all Python tests in the test directory.
 
+version="${1:-2}"
+
 for test in tests/*.py; do
-  echo "Running ${test} ... "
-  python "${test}" -v && true
+  echo "Running ${test} on Python version ${version} ..."
+
+  for encoding in en_US.UTF-8 C; do
+    echo "Testing encoding ${encoding} ..."
+    LC_ALL="${encoding}" LANG="${encoding}" python "${test}" -v
+  done
 done
