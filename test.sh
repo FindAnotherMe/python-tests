5eeefcbef881586ecc42b2439414273d20a9c6b5
Matt Clay <matt@mystile.com>
2016-02-10 02:02:58 -0800

    Fixed python version selection.

--- test.sh
+++ test.sh
@@ -1,13 +1,13 @@
 #!/bin/sh -e
 # Run all Python tests in the test directory.
 
-version="${1:-2}"
+version="${1}"
 
 for test in tests/*.py; do
-  echo "Running ${test} on Python version ${version} ..."
+  echo "Running ${test} on python${version} ..."
 
   for encoding in en_US.UTF-8 C; do
     echo "Testing encoding ${encoding} ..."
-    LC_ALL="${encoding}" LANG="${encoding}" python "${test}" -v
+    LC_ALL="${encoding}" LANG="${encoding}" "python${version}" "${test}" -v
   done
 done
